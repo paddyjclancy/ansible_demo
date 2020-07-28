cde6eb5a8784f81b92905a40d365b3df5a45a7c3
paddyjclancy <patrickclancy0605@gmail.com>
2020-07-28 18:00:12 +0100

    host echo silenced

--- ansible-provision.sh
+++ ansible-provision.sh
@@ -8,18 +8,18 @@ sudo apt-get update
 
 sudo apt-get install ansible -y
 
-sudo echo "[web]" >> /etc/ansible/hosts
+# sudo echo "[web]" >> /etc/ansible/hosts
 
-sudo echo "192.168.0.10 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant" >> /etc/ansible/hosts
+# sudo echo "192.168.0.10 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant" >> /etc/ansible/hosts
 
-sudo echo "[db]" >> /etc/ansible/hosts
+# sudo echo "[db]" >> /etc/ansible/hosts
 
-sudo echo "192.168.0.20 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant" >> /etc/ansible/hosts
+# sudo echo "192.168.0.20 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant" >> /etc/ansible/hosts
 
-sudo echo "[aws]" >> /etc/ansible/hosts
+# sudo echo "[aws]" >> /etc/ansible/hosts
 
-sudo echo "192.168.0.30 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant" >> /etc/ansible/hosts
+# sudo echo "192.168.0.30 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant" >> /etc/ansible/hosts
 
-sudo echo "host_key_checking = False" >> /etc/ansible/ansible.cfg
+# sudo echo "host_key_checking = False" >> /etc/ansible/ansible.cfg
 
 
